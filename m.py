d1c1b2339f7ddde63f6ccf797b9d48be766db716
Pumalido <79928792+Pumalido@users.noreply.github.com>
2024-09-30 17:24:21 +0300

    Create m.py

---
+++ m.py
@@ -0,0 +1,27 @@
+import re
+
+def check_password(password):
+    # Проверяем длину пароля
+    if len(password) < 8:
+        return False
+
+    # Проверяем наличие прописных и строчных букв
+    if not re.search('[A-Z]', password) or not re.search('[a-z]', password):
+        return False
+
+    # Проверяем наличие цифры
+    if not re.search('[0-9]', password):
+        return False
+
+    # Проверяем наличие специального символа
+    if not re.search('[@_!#$%^&*()+=]', password):
+        return False
+
+    return True
+
+# Считываем файл с паролями
+with open('passwords.txt', 'r') as file:
+    for password in file:
+        password = password.strip()
+        if check_password(password):
+            print(password)
